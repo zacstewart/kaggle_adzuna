80ffd3ec86dab32cfeca2dfb96491d1b22168c09
Zac Stewart <zgstewart@gmail.com>
2013-02-24 18:20:09 -0500

    Test the performance of several models

--- controller.py
+++ controller.py
@@ -2,8 +2,15 @@ import logging
 import gc
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.cross_validation import ShuffleSplit
+from sklearn.linear_model import *
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import GradientBoostingRegressor
 from datasets import *
+from utilities import *
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,6 +22,41 @@ rows = np.array(rows)
 n            = rows.shape[0]
 descriptions = rows[:, h['FullDescription']]
 salaries     = rows[:, h['SalaryNormalized']].astype(int)
+
+rs = ShuffleSplit(n, test_size=0.2, random_state=0)
+count_vectorizer = CountVectorizer()
+tfidf_transformer = TfidfTransformer()
+
+models = [
+    MultinomialNB(),
+    KNeighborsRegressor(),
+    SVR(),
+    LinearRegression(),
+    Ridge(),
+    Lasso(),
+    ElasticNet(),
+    SGDRegressor(),
+  ]
+
+model_scores = {}
+for model in models:
+  scores = []
+  for train_index, cv_index in rs:
+    X_train_counts = count_vectorizer.fit_transform(descriptions[train_index])
+    X_train = tfidf_transformer.fit_transform(X_train_counts)
+    y_train = salaries[train_index]
+
+    X_cv_counts = count_vectorizer.transform(descriptions[cv_index])
+    X_cv = tfidf_transformer.transform(X_cv_counts)
+    y_cv = salaries[cv_index]
+
+    clf = model.fit(X_train, y_train)
+    scores.append(clf.score(X_cv, y_cv))
+  model_scores[model.__class__.__name__] = mean(scores)
+
+for (model, score) in model_scores.iteritems():
+  print "%(model)s: %(score)f" % \
+      {'model': model, 'score': score}
 
 logging.info('Freeing test set memory...')
 del rows
